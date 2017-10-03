1ade1f04865e31a85135316c29d4697cba122089
Sandra Kuzmić <sandra.kuzmic@fer.hr>
2017-10-03 22:08:20 +0200

    allow restart of scheduler for widget update

--- app/src/main/java/com/sandra/tasky/TaskyConstants.java
+++ app/src/main/java/com/sandra/tasky/TaskyConstants.java
@@ -6,6 +6,7 @@ public class TaskyConstants {
     public static String PREFS_SHOW_EXPIRED = "pref_show_expired";
     public static String PREFS_TIME_SPAN = "pref_time_span";
     public static String PREFS_TIME_SPAN_DEFAULT = "-1";
+    public static String PREFS_RESTART_SCHEDULER = "pref_restart_scheduler";
 
     public static String WIDGET_UPDATE_ACTION = "com.sandra.tasky.widget.UPDATE_WIDGET";
     public static String WIDGET_FIRST_RUN = "first_run_of_widget_1.19";
--- app/src/main/java/com/sandra/tasky/activities/SettingsFragment.java
+++ app/src/main/java/com/sandra/tasky/activities/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.sandra.tasky.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 import com.sandra.tasky.R;
+import com.sandra.tasky.TaskyConstants;
 
 
 public class SettingsFragment extends PreferenceFragment {
@@ -12,5 +17,23 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference preference = findPreference(TaskyConstants.PREFS_RESTART_SCHEDULER);
+        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (!getActivity()
+                        .getSharedPreferences(TaskyConstants.WIDGET_FIRST_RUN, Context.MODE_PRIVATE)
+                        .getBoolean(TaskyConstants.PREFS_FIRST_RUN, true)) {
+                    SharedPreferences.Editor editor = getActivity().getSharedPreferences(TaskyConstants.WIDGET_FIRST_RUN, Context.MODE_PRIVATE).edit();
+                    editor.putBoolean(TaskyConstants.PREFS_FIRST_RUN, true);
+                    editor.apply();
+                    Toast.makeText(getActivity(), R.string.scheduler_restarted, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), R.string.please_restart_widget_to_complete, Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
     }
 }
