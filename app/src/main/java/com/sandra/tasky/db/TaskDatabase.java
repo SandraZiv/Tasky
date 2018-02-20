17425edac28dd649745de8a91554f4432fdcd661
Sandra Kuzmić <sandra.kuzmic@fer.hr>
2018-02-20 17:01:21 +0100

    refactor DB

--- app/src/main/java/com/sandra/tasky/db/TaskDatabase.java
+++ app/src/main/java/com/sandra/tasky/db/TaskDatabase.java
@@ -4,7 +4,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.sandra.tasky.R;
@@ -21,24 +20,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
+import static com.sandra.tasky.db.DatabaseConstants.FALSE;
+import static com.sandra.tasky.db.DatabaseConstants.TASKS_KEY_ID;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_COMPLETED_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_DATE_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_NOTE_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_SHOW_IN_WIDGET_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_TIME_PRESENT_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TASK_TITLE_COLUMN;
+import static com.sandra.tasky.db.DatabaseConstants.TRUE;
 
 public class TaskDatabase {
-
-    private static final int TRUE = 1;
-    private static final int FALSE = 0;
-
-    //creating table columns for tasks table
-    private static final String TASKS_KEY_ID = "ID";
-    private static final String TASK_TITLE_COLUMN = "TASK_TITLE_COLUMN";
-    private static final String TASK_COMPLETED_COLUMN = "TASK_COMPLETED_COLUMN";
-    private static final String TASK_NOTE_COLUMN = "TASK_NOTE_COLUMN";
-    private static final String TASK_DATE_COLUMN = "TASK_DATE_COLUMN";
-    private static final String TASK_TIME_PRESENT_COLUMN = "TASK_TIME_PRESENT_COLUMN";
-    private static final String TASK_SHOW_IN_WIDGET_COLUMN = "SHOW_IN_WIDGET_COLUMN";
-
-    //creating table columns for categories table
-    private static final String CATEGORIES_KEY_ID = "ID";
-    private static final String CATEGORIES_TITLE = "CATEGORIES_TITLE";
 
     private static TaskDatabaseOpenHelper taskDatabaseOpenHelper = null;
 
@@ -50,7 +42,7 @@ public class TaskDatabase {
     public TaskDatabase(Context context) {
         this.context = context;
 
-        taskDatabaseOpenHelper = TaskDatabaseOpenHelper.getInstance(context);
+        taskDatabaseOpenHelper = TaskDatabaseOpenHelper.getInstance(context, taskDatabaseOpenHelper);
 
         dbWritable = taskDatabaseOpenHelper.getWritableDatabase();
         dbReadable = taskDatabaseOpenHelper.getReadableDatabase();
@@ -186,60 +178,4 @@ public class TaskDatabase {
         }
         return new DateTime(date);
     }
-
-    private static class TaskDatabaseOpenHelper extends SQLiteOpenHelper {
-
-        static final String DATABASE_NAME = "task_database.db";
-        static final String DATABASE_TABLE_TASKS = "taskTable";
-        static final String DATABASE_TABLE_CATEGORIES = "categoriesTable";
-        static final int DATABASE_VERSION = 8;
-
-        static final String CREATE_TABLE_TASKS = "create table " + DATABASE_TABLE_TASKS + " ( "
-                + TASKS_KEY_ID + " integer primary key autoincrement,"
-                + TASK_TITLE_COLUMN + " text not null,"
-                + TASK_COMPLETED_COLUMN + " smallint check (" + TASK_COMPLETED_COLUMN + " in (0,1)),"
-                + TASK_NOTE_COLUMN + " text,"
-                + TASK_DATE_COLUMN + " timestamp,"
-                + TASK_TIME_PRESENT_COLUMN + " smallint check (" + TASK_TIME_PRESENT_COLUMN + " in (0,1)),"
-                + TASK_SHOW_IN_WIDGET_COLUMN + " smallint default " + TRUE
-                + ");";
-
-        static final String CREATE_TABLE_CATEGORIES = "create table " + DATABASE_TABLE_CATEGORIES + " ( "
-                + CATEGORIES_KEY_ID + " integer primary key autoincrement,"
-                + CATEGORIES_TITLE + " text not null"
-                + ");";
-
-        private static TaskDatabaseOpenHelper getInstance(Context context) {
-            if (taskDatabaseOpenHelper == null) {
-                taskDatabaseOpenHelper = new TaskDatabaseOpenHelper(context,
-                        TaskDatabaseOpenHelper.DATABASE_NAME,
-                        null,
-                        TaskDatabaseOpenHelper.DATABASE_VERSION);
-            }
-            return taskDatabaseOpenHelper;
-        }
-
-        private TaskDatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, name, factory, version);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL(CREATE_TABLE_TASKS);
-            db.execSQL(CREATE_TABLE_CATEGORIES);
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("alter table " + DATABASE_TABLE_TASKS + " rename to old");
-            db.execSQL(CREATE_TABLE_TASKS);
-            String columns = TASKS_KEY_ID + "," + TASK_TITLE_COLUMN + "," + TASK_COMPLETED_COLUMN + "," + TASK_NOTE_COLUMN + "," + TASK_DATE_COLUMN + "," + TASK_TIME_PRESENT_COLUMN;
-            db.execSQL("insert into " + DATABASE_TABLE_TASKS + "(" + columns + ")"
-                    + " select " + columns + " from old");
-            db.execSQL("drop table old");
-
-            db.execSQL(CREATE_TABLE_CATEGORIES);
-        }
-    }
-
 }
---
+++ app/src/main/java/com/sandra/tasky/db/TaskDatabaseOpenHelper.java
@@ -0,0 +1,62 @@
+package com.sandra.tasky.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.sandra.tasky.db.DatabaseConstants.*;
+
+class TaskDatabaseOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "task_database.db";
+    static final String DATABASE_TABLE_TASKS = "taskTable";
+    private static final String DATABASE_TABLE_CATEGORIES = "categoriesTable";
+    private static final int DATABASE_VERSION = 8;
+
+    private static final String CREATE_TABLE_TASKS = "create table " + DATABASE_TABLE_TASKS + " ( "
+            + TASKS_KEY_ID + " integer primary key autoincrement,"
+            + TASK_TITLE_COLUMN + " text not null,"
+            + TASK_COMPLETED_COLUMN + " smallint check (" + TASK_COMPLETED_COLUMN + " in (0,1)),"
+            + TASK_NOTE_COLUMN + " text,"
+            + TASK_DATE_COLUMN + " timestamp,"
+            + TASK_TIME_PRESENT_COLUMN + " smallint check (" + TASK_TIME_PRESENT_COLUMN + " in (0,1)),"
+            + TASK_SHOW_IN_WIDGET_COLUMN + " smallint default " + TRUE
+            + ");";
+
+    private static final String CREATE_TABLE_CATEGORIES = "create table " + DATABASE_TABLE_CATEGORIES + " ( "
+            + CATEGORIES_KEY_ID + " integer primary key autoincrement,"
+            + CATEGORIES_TITLE + " text not null"
+            + ");";
+
+    static TaskDatabaseOpenHelper getInstance(Context context, TaskDatabaseOpenHelper taskDatabaseOpenHelper) {
+        if (taskDatabaseOpenHelper == null) {
+            taskDatabaseOpenHelper = new TaskDatabaseOpenHelper(context,
+                    TaskDatabaseOpenHelper.DATABASE_NAME,
+                    null,
+                    TaskDatabaseOpenHelper.DATABASE_VERSION);
+        }
+        return taskDatabaseOpenHelper;
+    }
+
+    private TaskDatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_TASKS);
+        db.execSQL(CREATE_TABLE_CATEGORIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("alter table " + DATABASE_TABLE_TASKS + " rename to old");
+        db.execSQL(CREATE_TABLE_TASKS);
+        String columns = TASKS_KEY_ID + "," + TASK_TITLE_COLUMN + "," + TASK_COMPLETED_COLUMN + "," + TASK_NOTE_COLUMN + "," + TASK_DATE_COLUMN + "," + TASK_TIME_PRESENT_COLUMN;
+        db.execSQL("insert into " + DATABASE_TABLE_TASKS + "(" + columns + ")"
+                + " select " + columns + " from old");
+        db.execSQL("drop table old");
+
+        db.execSQL(CREATE_TABLE_CATEGORIES);
+    }
+}
