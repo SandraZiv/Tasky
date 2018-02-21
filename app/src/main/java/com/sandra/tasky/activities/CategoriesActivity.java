665237d90cc51a086d9f1b2ae1b6a81bff5e3f67
Sandra Kuzmić <sandra.kuzmic@fer.hr>
2018-02-21 17:34:35 +0100

    implement categories adding and deleting

--- app/src/main/java/com/sandra/tasky/activities/CategoriesActivity.java
+++ app/src/main/java/com/sandra/tasky/activities/CategoriesActivity.java
@@ -1,23 +1,42 @@
 package com.sandra.tasky.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.DataSetObserver;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.sandra.tasky.R;
+import com.sandra.tasky.TaskyUtils;
+import com.sandra.tasky.adapter.CategoriesAdapter;
+import com.sandra.tasky.db.TaskDatabase;
 import com.sandra.tasky.entity.TaskCategory;
 
 import java.util.List;
 
 public class CategoriesActivity extends AppCompatActivity {
 
+    private Toast mToast;
+    private CategoriesDataObserver observer;
+    private CategoriesAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        observer = new CategoriesDataObserver();
+
+        adapter = new CategoriesAdapter(this);
+        adapter.registerDataSetObserver(observer);
 
         new CategoryAsyncTask().execute(this);
     }
@@ -30,7 +49,51 @@ public class CategoriesActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.menu_add_category:
+                openNewCategoryDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        adapter.unregisterDataSetObserver(observer);
+    }
+
+    private void openNewCategoryDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.enter_title);
+
+        final EditText etTitle = new EditText(this);
+        etTitle.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(etTitle);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final String inputTitle = etTitle.getText().toString().trim();
+                if (inputTitle.isEmpty()) {
+                    mToast = TaskyUtils.addToast(mToast, CategoriesActivity.this, R.string.please_enter_title, true);
+                } else {
+                    //save new category
+                    TaskDatabase database = new TaskDatabase(CategoriesActivity.this);
+                    database.addCategory(new TaskCategory(inputTitle));
+                    //update UI
+                    new CategoryAsyncTask().execute(CategoriesActivity.this);
+                }
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
     private class CategoryAsyncTask extends AsyncTask<Context, Integer ,List<TaskCategory>> {
@@ -39,13 +102,26 @@ public class CategoriesActivity extends AppCompatActivity {
         @Override
         protected List<TaskCategory> doInBackground(Context... params) {
             this.context = params[0];
-
-            return null;
+            TaskDatabase database = new TaskDatabase(context);
+            return database.getAllCategories();
         }
 
         @Override
-        protected void onPostExecute(List<TaskCategory> taskCategories) {
-            super.onPostExecute(taskCategories);
+        protected void onPostExecute(List<TaskCategory> categories) {
+            adapter.setCategories(categories);
+
+            ListView listView = (ListView) findViewById(R.id.lv_categories);
+            listView.setAdapter(adapter);
+
+            listView.setEmptyView(findViewById(R.id.tv_no_categories));
+        }
+    }
+
+    private class CategoriesDataObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            new CategoryAsyncTask().execute(CategoriesActivity.this);
         }
     }
 }
--- app/src/main/java/com/sandra/tasky/adapter/CategoriesAdapter.java
+++ app/src/main/java/com/sandra/tasky/adapter/CategoriesAdapter.java
@@ -1,6 +1,7 @@
 package com.sandra.tasky.adapter;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,20 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.sandra.tasky.R;
+import com.sandra.tasky.db.TaskDatabase;
 import com.sandra.tasky.entity.TaskCategory;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CategoriesAdapter extends BaseAdapter {
     private Context context;
     private List<TaskCategory> categories;
+
+    public CategoriesAdapter(Context context) {
+        this.context = context;
+        this.categories = new LinkedList<>();
+    }
 
     public CategoriesAdapter(Context context, List<TaskCategory> categories) {
         this.context = context;
@@ -38,8 +46,8 @@ public class CategoriesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TaskCategory category = categories.get(position);
-        View view;
+        final TaskCategory category = categories.get(position);
+        final View view;
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
             view = convertView;
@@ -47,6 +55,35 @@ public class CategoriesAdapter extends BaseAdapter {
             view = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
         }
         ((TextView) view.findViewById(R.id.tv_category_title)).setText(category.getTitle());
+
+        view.findViewById(R.id.iv_category_delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CategoryAsyncTask().execute(category);
+            }
+        });
+
         return view;
+    }
+
+    public void setCategories(List<TaskCategory> categories) {
+        this.categories = categories;
+    }
+
+    private class CategoryAsyncTask extends AsyncTask<TaskCategory, Integer, List<TaskCategory>> {
+        private TaskCategory category;
+
+        @Override
+        protected List<TaskCategory> doInBackground(TaskCategory... params) {
+            this.category = params[0];
+            TaskDatabase database = new TaskDatabase(context);
+            database.deleteCategory(category);
+            return database.getAllCategories();
+        }
+
+        @Override
+        protected void onPostExecute(List<TaskCategory> categories) {
+            notifyDataSetChanged();
+        }
     }
 }
