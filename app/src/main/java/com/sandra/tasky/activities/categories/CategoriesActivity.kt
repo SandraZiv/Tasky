ec0e8918abdf19af38b96af2501e18d5e17bdfef
sandra_kuzmic <kuzmic.sandra@microblink.com>
2021-06-12 20:51:06 +0200

    fix categories dialog

--- app/src/main/java/com/sandra/tasky/activities/categories/CategoriesActivity.kt
+++ app/src/main/java/com/sandra/tasky/activities/categories/CategoriesActivity.kt
@@ -13,7 +13,7 @@ import com.sandra.tasky.db.DatabaseWrapper
 import com.sandra.tasky.entity.TaskCategory
 import com.sandra.tasky.utils.ToastWrapper
 import kotlinx.android.synthetic.main.activity_categories.*
-import kotlinx.android.synthetic.main.dialog_edit_text.*
+import kotlinx.android.synthetic.main.dialog_edit_text.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -68,9 +68,12 @@ class CategoriesActivity : AppCompatActivity() {
     private fun openNewCategoryDialog() {
         AlertDialog.Builder(this).apply {
             setTitle(R.string.enter_title)
-            setView(R.layout.dialog_edit_text)
+
+            val etView = layoutInflater.inflate(R.layout.dialog_edit_text, null)
+            setView(etView)
+
             setPositiveButton(R.string.ok) { _, _ ->
-                val inputTitle = etTitle.text.toString().trim { it <= ' ' }
+                val inputTitle = etView.etInput.text.toString().trim { it <= ' ' }
                 if (inputTitle.isEmpty()) {
                     ToastWrapper.showShort(this@CategoriesActivity, R.string.please_enter_title)
                 } else if (inputTitle.equals(getString(R.string.all), ignoreCase = true) || inputTitle.equals(getString(R.string.others), ignoreCase = true)) {
